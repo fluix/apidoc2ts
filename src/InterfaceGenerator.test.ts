2731ecf64a6f980d3dcb733b16262c981f4f9146
Artem Velkov <avelkov@readdle.com>
2019-07-17 15:19:34 +0300

    Add InterfaceGenerator

---
+++ src/InterfaceGenerator.test.ts
@@ -0,0 +1,102 @@
+import {InterfaceGenerator} from "./InterfaceGenerator";
+
+const simpleSchema = {
+    type: "object",
+    properties: {
+        param: {
+            type: "number",
+        },
+    },
+};
+
+const schemaWithOptionalAndRequiredParams = {
+    type: "object",
+    properties: {
+        optionalParam: {
+            type: "number",
+        },
+        requiredParam: {
+            type: "number",
+            required: true,
+        },
+    },
+};
+
+const schemaWithCustomType = {
+    type: "object",
+    properties: {
+        param: {
+            $ref: "#/definitions/User",
+        },
+    },
+    definitions: {
+        User: {
+            type: "object",
+            properties: {
+                prop: {},
+            },
+        },
+    },
+};
+
+const schemaWithEnum = {
+    type: "object",
+    properties: {
+        param: {
+            type: "string",
+            enum: ["a", "b", "c"],
+        },
+    },
+};
+
+describe("Interface generator", () => {
+    let generator: InterfaceGenerator;
+
+    beforeEach(() => {
+        generator = new InterfaceGenerator();
+    });
+
+    it("should return empty string if called with empty schema", async () => {
+        expect(await generator.createInterface({})).toBe("");
+    });
+
+    it("should create simple interface with one number property", async () => {
+        const result = await generator.createInterface(simpleSchema);
+        expect(result.includes("param?: number;")).toBeTruthy();
+    });
+
+    it("should create interface with given name", async () => {
+        const name = "User";
+        expect((await generator.createInterface(simpleSchema, name))
+            .includes(`interface ${name}`)).toBeTruthy();
+    });
+
+    it("should generate properties with corresponding optional state", async () => {
+        const interfaceString = await generator.createInterface(schemaWithOptionalAndRequiredParams);
+        expect((interfaceString).includes("optionalParam?:")).toBeTruthy();
+        expect((interfaceString).includes("requiredParam:")).toBeTruthy();
+    });
+
+    it("should generate enums", async () => {
+        const interfaceString = await generator.createInterface(schemaWithEnum);
+        expect((interfaceString).includes("export enum Param")).toBeTruthy();
+        expect((interfaceString).includes("param?: Param;")).toBeTruthy();
+    });
+
+    it("should generate properties with custom types", async () => {
+        const interfaceString = await generator.createInterface(schemaWithCustomType);
+        expect((interfaceString).includes("param?: User")).toBeTruthy();
+    });
+
+    it("should cleanup generated code", async () => {
+        const generatorWithCustomTypes = new InterfaceGenerator(["User"]);
+        const interfaceString = await generatorWithCustomTypes.createInterface(schemaWithCustomType);
+        expect((interfaceString).includes("interface User")).toBeFalsy();
+    });
+
+    it("should throw if interface name matches one of custom types", async () => {
+        const generatorWithCustomTypes = new InterfaceGenerator(["User"]);
+        await expect(generatorWithCustomTypes.createInterface(schemaWithCustomType, "User"))
+            .rejects.toThrow();
+    });
+});
---
+++ src/InterfaceGenerator.ts
@@ -0,0 +1,67 @@
+import {JsonSchema} from "./JsonSchema";
+import * as _ from "lodash";
+import {
+    InputData,
+    JSONSchemaInput,
+    JSONSchemaSourceData,
+    Options,
+    quicktype,
+    TypeScriptTargetLanguage,
+} from "quicktype-core";
+
+export class InterfaceGenerator {
+
+    private readonly customTypes: Array<string> = [];
+
+    private rendererOptions = {"just-types": "true"};
+    private targetLang = new TypeScriptTargetLanguage();
+
+    private qtMakeInput = () => new JSONSchemaInput(undefined);
+
+    constructor(customTypes: Array<string> = []) {
+        this.customTypes = customTypes;
+    }
+
+    async createInterface(schema: JsonSchema, name = "Generated") {
+        if (_.isEmpty(schema)) {
+            return "";
+        }
+
+        if (this.customTypes.includes(name)) {
+            throw new Error(`Interface ${name} exists in custom types: [${this.customTypes.join(",")}]`);
+        }
+
+        const inputData = this.createInputData(schema, name);
+        const quicktypeOptions = this.createOptions(inputData);
+        const interfaceString = await this.execQuicktypeGenerator(quicktypeOptions);
+        return this.removeFakeDefinitions(interfaceString);
+    }
+
+    private async execQuicktypeGenerator(quicktypeOptions) {
+        const result = await quicktype(quicktypeOptions);
+        return result.lines.join("");
+    }
+
+    private createInputData(schema: JsonSchema, name: string) {
+        const schemaString = JSON.stringify(schema);
+        const source: JSONSchemaSourceData = {name, schema: schemaString};
+        const inputData = new InputData();
+
+        inputData.addSourceSync("schema", source, this.qtMakeInput);
+        return inputData;
+    }
+
+    private createOptions(inputData: InputData): Partial<Options> {
+        return {
+            inputData,
+            lang: this.targetLang,
+            rendererOptions: this.rendererOptions,
+        };
+    }
+
+    private removeFakeDefinitions(interfaceString: string): string {
+        const regexpInterfaceNames = this.customTypes.join("|");
+        const regexp = new RegExp(`(export interface (${regexpInterfaceNames}) {[^}]*})`);
+        return interfaceString.replace(regexp, "");
+    }
+}
