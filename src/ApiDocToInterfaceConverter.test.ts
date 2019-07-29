d16c119163253a5461286c2486e5c0cdcfa8309c
Artem Velkov <avelkov@readdle.com>
2019-07-29 13:19:47 +0300

    Remove unnecessary variables from tests

--- src/ApiDocToInterfaceConverter.test.ts
+++ src/ApiDocToInterfaceConverter.test.ts
@@ -2,85 +2,6 @@ import {ApiDocToInterfaceConverter} from "./ApiDocToInterfaceConverter";
 import {InterfaceGenerator} from "./InterfaceGenerator";
 import {ApiDocEndpointParser} from "./ApiDocEndpointParser";
 
-const getRequestData = {
-    type: "get",
-    url: "/user/:id",
-    title: "Read data of a User",
-    version: "0.3.0",
-    name: "GetUser",
-    group: "User",
-    permission: {
-        name: "admin",
-        title: "Admin access rights needed. ",
-        description: "Permission description",
-    },
-    description: "<p>Interface description</p>",
-    parameter: {
-        fields: {
-            Parameter: [
-                {
-                    group: "Parameter",
-                    type: "string",
-                    field: "id",
-                    optional: false,
-                    description: "<p>The Users-ID.</p>",
-                },
-            ],
-        },
-    },
-    filename: "source/example_full/example.js",
-};
-
-const postRequestData = {
-    type: "post",
-    url: "/user",
-    title: "Create a new User",
-    version: "0.3.0",
-    name: "PostUser",
-    group: "User",
-    permission: "none",
-    description: "<p>Interface description</p>",
-    parameter: {
-        fields: {
-            Parameter: [
-                {
-                    group: "Parameter",
-                    type: "string",
-                    field: "name",
-                    optional: false,
-                    description: "<p>Name of the User.</p>",
-                },
-            ],
-        },
-    },
-    filename: "source/example_full/example.js",
-};
-
-const postRequestDataWithCustomTypes = {
-    type: "post",
-    url: "/user",
-    title: "Create a new User",
-    version: "0.3.0",
-    name: "CreateUser",
-    group: "User",
-    permission: "none",
-    description: "<p>Interface description</p>",
-    parameter: {
-        fields: {
-            Parameter: [
-                {
-                    group: "Parameter",
-                    type: "User",
-                    field: "user",
-                    optional: false,
-                    description: "<p>User</p>",
-                },
-            ],
-        },
-    },
-    filename: "source/example_full/example.js",
-};
-
 const requestVersion1 = {
     type: "post",
     url: "/user",
@@ -141,7 +62,7 @@ const requestVersion3 = {
 jest.mock("./ApiDocEndpointParser");
 jest.mock("./InterfaceGenerator");
 
-const apiDocDataFull = [getRequestData, postRequestData, postRequestDataWithCustomTypes];
+const apiDocDataFull = [requestVersion1, requestVersion2, requestVersion3];
 
 const parserResultMock = {
     request: {type: "requestMock"},
@@ -150,17 +71,17 @@ const parserResultMock = {
 };
 
 describe("ApiDoc to Interface converter", () => {
+    const interfaceGenerator = new InterfaceGenerator();
     const apiDocEndpoint: ApiDocEndpointParser = new ApiDocEndpointParser();
     const parseEndpointSpy = jest.spyOn(apiDocEndpoint, "parseEndpoint");
 
-    let interfaceGenerator: InterfaceGenerator;
     let converter: ApiDocToInterfaceConverter;
 
     beforeEach(() => {
+        (interfaceGenerator.createInterface as jest.Mock).mockReset();
         parseEndpointSpy.mockReset();
         parseEndpointSpy.mockImplementation(() => parserResultMock);
 
-        interfaceGenerator = new InterfaceGenerator(["User"]);
         converter = new ApiDocToInterfaceConverter(interfaceGenerator, apiDocEndpoint);
     });
 
@@ -169,22 +90,22 @@ describe("ApiDoc to Interface converter", () => {
     });
 
     it("should call parseEndpoint with apiDoc data", async () => {
-        await converter.convert([getRequestData]);
-        expect(parseEndpointSpy).toBeCalledWith(getRequestData);
+        await converter.convert([requestVersion1]);
+        expect(parseEndpointSpy).toBeCalledWith(requestVersion1);
     });
 
     it("should call createInterface with parsed schemas for request, response and error", async () => {
-        await converter.convert([getRequestData]);
+        await converter.convert([requestVersion1]);
         expect(interfaceGenerator.createInterface).toBeCalledWith(parserResultMock.request, expect.anything());
         expect(interfaceGenerator.createInterface).toBeCalledWith(parserResultMock.response, expect.anything());
         expect(interfaceGenerator.createInterface).toBeCalledWith(parserResultMock.error, expect.anything());
     });
 
     it("should call createInterface with name from apiDoc endpoint and default postfixes", async () => {
-        await converter.convert([getRequestData]);
-        expect(interfaceGenerator.createInterface).toBeCalledWith(expect.anything(), getRequestData.name);
-        expect(interfaceGenerator.createInterface).toBeCalledWith(expect.anything(), `${getRequestData.name}Response`);
-        expect(interfaceGenerator.createInterface).toBeCalledWith(expect.anything(), `${getRequestData.name}Error`);
+        await converter.convert([requestVersion1]);
+        expect(interfaceGenerator.createInterface).toBeCalledWith(expect.anything(), requestVersion1.name);
+        expect(interfaceGenerator.createInterface).toBeCalledWith(expect.anything(), `${requestVersion1.name}Response`);
+        expect(interfaceGenerator.createInterface).toBeCalledWith(expect.anything(), `${requestVersion1.name}Error`);
     });
 
     it("should call parseEndpoint and createInterface for every endpoint", async () => {
@@ -207,7 +128,7 @@ describe("ApiDoc to Interface converter", () => {
         parseEndpointSpy.mockImplementationOnce(() => {
             throw new Error("Mocked error while parsing");
         });
-        const results = await converter.convert([getRequestData, postRequestData]);
+        const results = await converter.convert([requestVersion1, requestVersion2]);
         expect(results[0].warning).toBeDefined();
         expect(results[1].warning).toBeUndefined();
     });
