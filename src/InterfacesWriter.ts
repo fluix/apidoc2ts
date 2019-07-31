80040c0a5683c422de3e596e577876e6865be76c
Artem Velkov <avelkov@readdle.com>
2019-07-31 12:55:55 +0300

    Change util lambdas to functions

--- src/InterfacesWriter.ts
+++ src/InterfacesWriter.ts
@@ -11,25 +11,25 @@ export interface InterfacesWriter {
     ): Promise<void | Array<void>>;
 }
 
-export const getInterfaceWriter = (groupingMode: ApiDoc2InterfaceGroupingMode): InterfacesWriter => {
+export function getInterfaceWriter(groupingMode: ApiDoc2InterfaceGroupingMode): InterfacesWriter {
     if (groupingMode === ApiDoc2InterfaceGroupingMode.URL) {
         return new UrlStructureInterfacesWriter();
     }
 
     return new SingleFileInterfacesWriter();
-};
+}
 
-export const stringifyAllInterfaces = (converterResults: Array<ConverterResult>): string => {
+export function stringifyAllInterfaces(converterResults: Array<ConverterResult>): string {
     return converterResults
         .map((endpointData) => this.stringifyInterfaces(endpointData))
         .filter(filterEmptyStrings)
         .join("\n");
-};
+}
 
-export const stringifyInterfaces = (converterResult: ConverterResult): string => {
+export function stringifyInterfaces(converterResult: ConverterResult): string {
     return [
         converterResult.requestInterface,
         converterResult.responseInterface,
         converterResult.errorInterface,
     ].filter(filterEmptyStrings).join("\n");
-};
+}
--- src/fs-utils.ts
+++ src/fs-utils.ts
@@ -5,7 +5,7 @@ import {promisify} from "util";
 const writeFile = promisify(fs.writeFile);
 const mkdir = promisify(fs.mkdir);
 
-export const writeFileToPath = (filePath, interfacesString): Promise<void> => {
+export function writeFileToPath(filePath, interfacesString): Promise<void> {
     return mkdir(path.dirname(filePath), {recursive: true})
         .then(() => {
             return writeFile(filePath, interfacesString);
@@ -13,4 +13,4 @@ export const writeFileToPath = (filePath, interfacesString): Promise<void> => {
         .catch((err) => {
             return Promise.reject(err);
         });
-};
+}
