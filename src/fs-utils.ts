05e2d3972e6c223ee87b69dd6a1b82bce571e4a8
Artem Velkov <avelkov@readdle.com>
2019-07-31 12:59:54 +0300

    Add types and options to util function

--- src/fs-utils.ts
+++ src/fs-utils.ts
@@ -1,14 +1,15 @@
 import * as fs from "fs";
 import * as path from "path";
 import {promisify} from "util";
+import {WriteFileOptions} from "fs";
 
 const writeFile = promisify(fs.writeFile);
 const mkdir = promisify(fs.mkdir);
 
-export function writeFileToPath(filePath, interfacesString): Promise<void> {
+export function writeFileToPath(filePath: string, data: any, options?: WriteFileOptions): Promise<void> {
     return mkdir(path.dirname(filePath), {recursive: true})
         .then(() => {
-            return writeFile(filePath, interfacesString);
+            return writeFile(filePath, data, options);
         })
         .catch((err) => {
             return Promise.reject(err);
