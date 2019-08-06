c727141f8aa009a30c75a66c5a95fa219403ee41
Artem Velkov <avelkov@readdle.com>
2019-08-06 16:42:57 +0300

    Cleanup code
    - rename config filename variable
    - remove unnecessary ?:
    - remove unnecessary type declaration by moving it to methods

--- src/cli/InputParser.ts
+++ src/cli/InputParser.ts
@@ -1,9 +1,9 @@
 import Convert = require("./index");
-import {BuilderOptions} from "../core/ApiDoc2InterfaceBuilder";
-import {ApiDoc2InterfaceParameters} from "../core/ApiDoc2Interface";
-import * as path from "path";
 import * as fs from "fs";
 import * as _ from "lodash";
+import * as path from "path";
+import {ApiDoc2InterfaceParameters} from "../core/ApiDoc2Interface";
+import {BuilderOptions} from "../core/ApiDoc2InterfaceBuilder";
 
 type CLIFlags = Record<keyof typeof Convert.flags, any>;
 interface ConfigFlags extends BuilderOptions, ApiDoc2InterfaceParameters {}
@@ -11,14 +11,14 @@ interface ConfigFlags extends BuilderOptions, ApiDoc2InterfaceParameters {}
 export class InputParser {
 
     static requiredFlagsKeys: Array<keyof CLIFlags> = ["source", "output", "name"];
-    static configFileName = "apidoc2ts.config.js";
+    static defaultConfigFileName = "apidoc2ts.config.js";
 
     async parse(flags: Partial<CLIFlags>): Promise<{
         builderOptions: Partial<BuilderOptions>,
         runParameters: ApiDoc2InterfaceParameters,
     }> {
-        const configFlags: ConfigFlags = this.getConfigParameters(flags.config);
-        const mappedInputFlags: ConfigFlags = this.mapInputFlags(flags);
+        const configFlags = this.getConfigParameters(flags.config);
+        const mappedInputFlags = this.mapInputFlags(flags);
         const combinedFlags = _.defaults(mappedInputFlags, configFlags);
 
         this.validateInput(combinedFlags);
@@ -29,10 +29,8 @@ export class InputParser {
         };
     }
 
-    private getConfigParameters(configPath: string | undefined) {
-        return configPath
-               ? this.readConfigFlags(configPath)
-               : this.readConfigFlags(InputParser.configFileName);
+    private getConfigParameters(configPath: string | undefined): ConfigFlags {
+        return this.readConfigFlags(configPath || InputParser.defaultConfigFileName);
     }
 
     private readConfigFlags(config: string) {
