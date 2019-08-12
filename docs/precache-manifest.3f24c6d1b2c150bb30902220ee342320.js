a6a51380853fb388d9c43faadb5178fe3e6f29bc
Artem Velkov <avelkov@readdle.com>
2019-08-12 16:22:08 +0300

    Add react based static SPA

---
+++ docs/precache-manifest.3f24c6d1b2c150bb30902220ee342320.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a14a5d025735c8b22735fb23a57a6398",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8772bec883b7a03d9e1",
+    "url": "/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "e369e9d4155c1ba029e9",
+    "url": "/static/js/2.f24fecea.chunk.js"
+  },
+  {
+    "revision": "b8772bec883b7a03d9e1",
+    "url": "/static/js/main.ad0aa18a.chunk.js"
+  },
+  {
+    "revision": "103cb5655188eb85587c",
+    "url": "/static/js/runtime~main.63753328.js"
+  }
+]);
