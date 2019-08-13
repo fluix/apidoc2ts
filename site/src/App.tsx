3365a6537fa0cfd9698e1a09663f225f5b99dc73
Artem Velkov <avelkov@readdle.com>
2019-08-13 14:21:22 +0300

    Add quick start component

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./components/header/header";
+import QuickStart from "./components/quick-links/quick-start";
 import Title from "./components/title/title";
 import template_image from "./images/gray-box.png";
 
@@ -10,6 +11,11 @@ const App: React.FC = () => {
             <Title
                 text="Get rid of boring day-to-day manual retyping and mechanical mistakes"
                 image={template_image}
+            />
+            <QuickStart
+                installCommand="$ npm install apidoc2ts -g"
+                githubLink="https://github.com/fluix/web-apidoc2ts"
+                npmLink="https://www.npmjs.com/package/apidoc2ts"
             />
         </>
     );
