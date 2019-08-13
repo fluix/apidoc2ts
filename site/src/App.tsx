c9d2117fe674135fc409626fb44cd1a0c742539c
Artem Velkov <avelkov@readdle.com>
2019-08-13 18:06:44 +0300

    Extract features data and inline map

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -3,43 +3,10 @@ import Feature from "./components/feature/feature";
 import Header from "./components/header/header";
 import QuickStart from "./components/quick-links/quick-start";
 import Title from "./components/title/title";
+import {features} from "./features";
 import template_image from "./images/gray-box.png";
 
 const App: React.FC = () => {
-    const featuresData = [
-        {
-            description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Aenean tincidunt metus ac ligula tempor, eget lobortis velit blandit.
-                Ut suscipit augue eget nibh rutrum, non lobortis turpis mattis.
-                Cras vel mauris faucibus, hendrerit ex non, vehicula massa.
-                Nullam iaculis turpis eget leo mollis finibus.`,
-            image: template_image,
-            imageOnLeft: true,
-        },
-        {
-            description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Aenean tincidunt metus ac ligula tempor, eget lobortis velit blandit.
-                Ut suscipit augue eget nibh rutrum, non lobortis turpis mattis.
-                Cras vel mauris faucibus, hendrerit ex non, vehicula massa.
-                Nullam iaculis turpis eget leo mollis finibus.`,
-            image: template_image,
-            imageOnLeft: false,
-        },
-        {
-            description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Aenean tincidunt metus ac ligula tempor, eget lobortis velit blandit.
-                Ut suscipit augue eget nibh rutrum, non lobortis turpis mattis.
-                Cras vel mauris faucibus, hendrerit ex non, vehicula massa.
-                Nullam iaculis turpis eget leo mollis finibus.`,
-            image: template_image,
-            imageOnLeft: true,
-        },
-    ];
-
-    const features = featuresData.map((data, index) => (
-        <Feature image={data.image} description={data.description} imageOnLeft={data.imageOnLeft} key={index}/>
-    ));
-
     return (
         <>
             <Header>ApiDoc2ts</Header>
@@ -52,7 +19,13 @@ const App: React.FC = () => {
                 githubLink="https://github.com/fluix/web-apidoc2ts"
                 npmLink="https://www.npmjs.com/package/apidoc2ts"
             />
-            {features}
+            {features.map((feature, index) => (
+                <Feature image={feature.image}
+                         description={feature.description}
+                         imageOnLeft={feature.imageOnLeft}
+                         key={index}
+                />
+            ))}
         </>
     );
 };
