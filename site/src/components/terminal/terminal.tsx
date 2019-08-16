d2ae1b53ba3bd4d384f0ed7fde8cdac2068205e4
Artem Velkov <avelkov@readdle.com>
2019-08-16 15:39:56 +0300

    Extract terminal lines of different types into components

--- site/src/components/terminal/terminal.tsx
+++ site/src/components/terminal/terminal.tsx
@@ -17,24 +17,38 @@ export interface TerminalLine {
 export default class Terminal extends PureComponent<TerminalProps, {}> {
     render() {
         const lines = this.props.lines.map((line, index) => {
-            const modifier = line.type === "command" ? "" : "terminal__line--response";
-            const linePrefix = line.type === "command" ? "$ " : "";
-
-            return <pre className={`terminal__line ${modifier}`}
-                        key={index}>
-                {linePrefix}{line.text}
-            </pre>;
+            return line.type === "command"
+                   ? <TerminalLineCommand key={index}>{line.text}</TerminalLineCommand>
+                   : <TerminalLineResponse key={index}>{line.text}</TerminalLineResponse>;
         });
 
         return (
             <div className="terminal">
                 <ContentWrapper>
                     <div className="terminal__window">
-                        <img className="terminal__controls-image" src={controlsImage} alt="terminal controls"/>
+                        <img className="terminal__controls-image"
+                             src={controlsImage}
+                             alt="terminal controls"/>
                         <div>{lines}</div>
                     </div>
                 </ContentWrapper>
             </div>
         );
+    }
+}
+
+class TerminalLineCommand extends PureComponent {
+    render() {
+        return <pre className="terminal__line">
+            $ {this.props.children}
+        </pre>;
+    }
+}
+
+class TerminalLineResponse extends PureComponent {
+    render() {
+        return <pre className="terminal__line terminal__line--response">
+            {this.props.children}
+        </pre>;
     }
 }
